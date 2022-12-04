a873de2c72b50ccc8a39db60abded07ace04371a
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2022-12-04 10:19:23 +0100

    Requirements for the app

--- e-commerce.js
+++ e-commerce.js
@@ -1,9 +1,19 @@
 const prompt = require("prompt-sync")({sigint: true});
 
-const name = prompt(`What is your name?`);
-console.log(`Your name is ${name}`);
+const cart = new Map();
 
-//Load cart
-//display cart
-//display aisle
-//
+//manage order: does the following:
+//add to cart
+//remove from cart
+//checkout: asks for an address, a rider will be with you, thanks for shopping
+//view cart
+
+//display aisle: displays aisle and returns aisle number
+
+//display products: accpets aisle number which serves as a key, then displays the values as 
+//products, returns the key and price
+
+//display cart: a proper view of cart with total amount, discount and tax
+
+//calculate total amount and discount and tax: 
+
