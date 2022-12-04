f904a5db67bd5bad7aabadca98653bc2c0ddf36d
Adaobi Chuks-Aginam <chuksaginamada@gmail.com>
2022-12-04 18:29:53 +0100

    Case a for manageOrder function done

--- e-commerce.js
+++ e-commerce.js
@@ -2,15 +2,15 @@ const prompt = require("prompt-sync")({sigint: true});
 
 const cart = [];
 
-cart.push({
-    name: "Pepsi",
-    amount: 200,
-    quantity: 4,
-}, {
-    name: "Coke",
-    amount: 200,
-    quantity: 4,
-})
+// cart.push({
+//     name: "Pepsi",
+//     amount: 200,
+//     quantity: 4,
+// }, {
+//     name: "Coke",
+//     amount: 200,
+//     quantity: 4,
+// })
 
 const products = new Map();
 
@@ -20,31 +20,31 @@ products.set("Beverages", [["Coke", 200, 4], ["Fanta", 200, 4], ["Pepsi", 200, 4
 
 const displayCategory = () => {
     let category = "";
-    let count = 0;
+    let count = 1;
     for (const key of products.keys()) {
-        category += `${count + 1}.\t${key}\n`;
+        category += `${count}. ${key}\n`;
         count++;
     }
     return category;
 }
 
-// console.log(displayCategory())
-
 const displayProducts = (num = 0) => {
-    if (num <= 0 || num > 3) {
+    let count = 1;
+    if (num < 0 || num > 3) {
         return `Please input a valid Aisle or Product`
     } else {
-        const key = Array.from(products.keys())[num];
+        const key = Array.from(products.keys())[num - 1];
         const values = products.get(key);
         let toStringProduct = "";
         values.forEach(value => {
-            toStringProduct += `\n${value[0]}\t\t\t${value[2]}\t\t  ${value[1]}`
+            toStringProduct += `\n${count}.\t${value[0]}\t\t${value[2]}\t\t  ${value[1]}`
+            count++;
         })
-        return `Product\t\tQuantity\tAmount` + toStringProduct;
+        return `\tProducts\tQuantity\tAmount` + toStringProduct;
     }
 }
 
-console.log(displayProducts());
+// console.log(displayProducts());
 
 const calculateTotal = () => {
     let totalAmount = 0;
@@ -80,18 +80,19 @@ const calculateTax = () => {
     return total * 0.1;
 }
 
-console.log(calculateTax());
+// console.log(calculateTax());
 
 const discount = calculateDiscount();
 
 const displayCart = () => {
-    let toStringProduct = "";
+    let toString = "";
     cart.forEach(product => {
-        toStringProduct += `\n${product.name}\t\t\t${product.quantity}\t\t  ${product.amount}`
+        toString += `\n${product[0]}\t\t\t${1}\t\t  ${product[1]}`
     })
-    return `Product\t\tQuantity\tAmount` + toStringProduct + 
+    return "**************************************CART**************************************" 
+            + `\nProduct\tQuantity\tAmount` + toString + 
             `\n\n\tTotal: ${total}` +
-            `\nYou are eligible for ${discount}% dicount`;
+            `\nYou are eligible for ${discount}% dicount\n`;
 }
 
 // console.log(displayCart());
@@ -103,4 +104,35 @@ const displayCart = () => {
 //checkout: asks for an address, a rider will be with you, thanks for shopping
 //view cart
 
-
+const manageOrders = () => {
+    console.log("\n*********************************************************************************");
+    console.log("********************************Genesys Groceries********************************");
+    console.log("*********************************************************************************\n");
+    const name = prompt("Please input your name to gain access to your personal cart: ").toUpperCase();
+    console.log(`\n\tWelcome ${name}!!!\n\n\tSo pleased to have you here ${name}.\n`);
+    while (true) {
+        console.log("What action would you like to perform?\nOptions: \n\ta) Add to cart\n\tb) Remove from cart\n\tc) Checkout");
+        const option = prompt().toLowerCase();
+        switch(option) {
+            case "a":
+                console.log(`\n${displayCategory()}`);
+                console.log("Choose a category to shop for between: 1 and 3 above\n\t");
+                const category = prompt();
+                console.log(displayProducts(category) + "\n\nChoose an item to add to your cart between: 1 and 3 above\n\t");
+                const item= prompt();
+                const key = Array.from(products.keys())[category - 1];
+                cart.push(products.get(key)[item - 1]);
+                break;
+            case "b":
+                break;
+            case "c":
+                const option = prompt("Please input your address: ");
+                break;
+            default:
+                break;
+        }
+        console.log(displayCart());
+        prompt("Press enter to continue!\n");
+    }
+}
+manageOrders();
